14c26d2fbefb5f60f6f82858892a93e570cbe8ef
Ayaad <96831865+Ayaad72@users.noreply.github.com>
2022-04-07 08:43:13 -0700

    Add files via upload

---
+++ DOMS/COUNTER.js
@@ -0,0 +1,25 @@
+
+// const btn = document.querySelector('.CLICK')
+// let counting =0;
+// btn.addEventListener('click', ()=>{
+//     counting ++;
+//     console.log(counting)
+// })
+
+let inc = 0;
+
+function Add(){
+    document.getElementById('result').value = ++inc;
+}
+
+function Sub(){
+    document.getElementById('result').value = --inc;
+}
+function Remove() {
+    document.getElementById('result').value = '0'
+}
+
+
+
+
+
